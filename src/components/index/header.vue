e9bd6f4cad29c257e961da78f4317b59c1cb75e8
nczkevin <z1376871863@163.com>
2016-07-26 18:03:33 +0800

    nav

---
+++ src/components/index/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">
+          <img alt="Brand" src="../../../static/img/youth.png">
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<style>
+.navbar {
+  background-color: #891619;
+  height: 80px;
+}
+.navbar-brand > img {
+  text-align: center;
+}
+</style>
